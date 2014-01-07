67e5d0c9f81e45b5b5f05aad3a5d97a7cb790795
marcotesta <mrc.tst@gmail.com>
2014-01-07 13:40:04 +0100

    Standardize license terms

--- src/src_data_adapter/ExternalFeed.cs
+++ src/src_data_adapter/ExternalFeed.cs
@@ -1,3 +1,4 @@
+#region License
 /*
  *
  * Copyright 2013 Weswit s.r.l.
@@ -15,6 +16,7 @@
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#endregion License
 
 using System;
 using System.Threading;
